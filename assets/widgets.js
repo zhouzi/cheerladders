69f63863a1a38949d753f03c7b0f2512d02b6f9b
Gabin <hello@gabinaureche.com>
2017-03-07 22:20:35 +0100

    add util to avoid loading twice a third party lib

--- assets/widgets.js
+++ assets/widgets.js
@@ -1,6 +1,25 @@
 (function() {
   'use strict';
 
+  var loadTwitterWidget = once(function() {
+    window.twttr = (function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0],
+        t = window.twttr || {};
+      if (d.getElementById(id)) return t;
+      js = d.createElement(s);
+      js.id = id;
+      js.src = "https://platform.twitter.com/widgets.js";
+      fjs.parentNode.insertBefore(js, fjs);
+
+      t._e = [];
+      t.ready = function(f) {
+        t._e.push(f);
+      };
+
+      return t;
+    }(document, "script", "twitter-wjs"));
+  });
+
   var widgets = {
     website: {
       name: 'Visit our website',
@@ -18,22 +37,7 @@
       },
       render: function(wrapper, params) {
         wrapper.innerHTML = '<a class="twitter-follow-button" href="https://twitter.com/' + params.handle + '" target="_blank">Follow @' + params.handle + '</a>';
-        window.twttr = (function(d, s, id) {
-          var js, fjs = d.getElementsByTagName(s)[0],
-            t = window.twttr || {};
-          if (d.getElementById(id)) return t;
-          js = d.createElement(s);
-          js.id = id;
-          js.src = "https://platform.twitter.com/widgets.js";
-          fjs.parentNode.insertBefore(js, fjs);
-
-          t._e = [];
-          t.ready = function(f) {
-            t._e.push(f);
-          };
-
-          return t;
-        }(document, "script", "twitter-wjs"));
+        loadTwitterWidget();
       },
     },
     tweet: {
@@ -43,25 +47,21 @@
       },
       render: function(wrapper, params) {
         wrapper.innerHTML = '<a class="twitter-share-button" href="https://twitter.com/intent/tweet?text=' + encodeURIComponent(params.content) + '" target="_blank">Tweet</a>';
-        window.twttr = (function(d, s, id) {
-          var js, fjs = d.getElementsByTagName(s)[0],
-            t = window.twttr || {};
-          if (d.getElementById(id)) return t;
-          js = d.createElement(s);
-          js.id = id;
-          js.src = "https://platform.twitter.com/widgets.js";
-          fjs.parentNode.insertBefore(js, fjs);
-
-          t._e = [];
-          t.ready = function(f) {
-            t._e.push(f);
-          };
-
-          return t;
-        }(document, "script", "twitter-wjs"));
+        loadTwitterWidget();
       },
     },
   };
+
+  function once(fn) {
+    var called = false;
+
+    return function() {
+      if (!called) {
+        called = true;
+        return fn.apply(null, arguments);
+      }
+    };
+  }
 
   if (typeof module === 'undefined') {
     window.widgets = widgets;
